0faae1b8be802930acac8c1e497698fd0a539569
MirAshique <ashiqtalpur18.com>
2026-02-12 17:16:06 +0500

    Frontend production config

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
-
 // Automatically attach token to every request
 API.interceptors.request.use(
   (req) => {
